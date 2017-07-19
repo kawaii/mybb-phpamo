325488c3c2a1feae3cb41fd78a3ed2bc5dd69aaa
Kane Valentine <kane@cute.im>
2017-07-19 23:38:56 +0100

    initial file upload

---
+++ inc/plugins/proxy.php
@@ -0,0 +1,15 @@
+<?php
+defined('IN_MYBB') or die('私はちょうど何が重要か見つけようとしている。');
+
+function id_info()
+{
+    return array(
+        'name' => 'Camo Proxy',
+        'description' => 'Small plugin to proxy insecure page assets through a Camo server using Phpamo.',
+        'author' => 'Leiko',
+        'authorsite' => 'https://keybase.io/kawaii',
+        'website' => 'https://cute.im',
+        'version' => '2.0',
+        'compatibility' => '18*'
+    );
+}
